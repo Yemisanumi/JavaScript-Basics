fcf1bc860fe97f842b16e5f37a7c60ec44674d50
Yemisanumi <oluwayemisanumi@gmail.com>
2023-12-05 06:30:23 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//Assignment Number 1
+function evenNum (number){
+    return number % 2 ===0;   
+}
+console.log (evenNum (20))
+
+//Assignment Number 2
+function printNumbers (n1, n2) {
+    for (let i = n1; i <= n2; i++){
+        console.log (i)
+    }
+}
+printNumbers (40, 50)
+
+//Assignment Number 3
+function sumValues (n1, n2){
+    let sum =0;
+    for (let i = n1; i <=n2; i++){
+        sum += i;
+    }
+    return sum;
+}
+console.log (sumValues (10, 20))
+
+
+
+
+
+   
